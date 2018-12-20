a9b63a26b44af4ecf52a1c7b98cc9d3db2cdf185
dreamxinxcode <42306596+dreamxinxcode@users.noreply.github.com>
2018-12-19 23:09:16 -0800

    Add files via upload

--- color-saver.py
+++ color-saver.py
@@ -7,7 +7,7 @@ image = Image.new("RGB", (500, 500), hex)
 image.paste(hex, [0,0,image.size[0],image.size[1]])
 
 draw = ImageDraw.Draw(image)
-font = ImageFont.truetype("UBraille.ttf", 16)
-draw.text((20, 20),"Sample Text",(0,0,255),font=font)
+font = ImageFont.truetype("LiberationSans-Bold.ttf", 30)
+draw.text((20, 20), hex,(255, 255, 255),font=font)
 
 image.save(hex + ".png")
